cf55ed605343bad80595280abe553437f0ceb6d1
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-09-19 15:46:15 -0400

    refactor done, mvp met

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -24,13 +24,71 @@ class CharacterStats extends GameObject {
 
 class Humanoid extends CharacterStats {
     constructor(character) {
-        super(character)
+        super(character);
         this.team = character.team;
         this.weapons = character.weapons;
         this.language = character.language;
     }
     greet() {
         return `${this.name} offers a greeing in ${this.language}.`;
+    }
+}
+
+class Person {
+    constructor(baseInfo) {
+        this.name = baseInfo.name;
+        this.age = baseInfo.age;
+        this.location = baseInfo.location;
+    }
+    speak() {
+        return `Hello, my name is ${this.name}.  I am from ${this.location}.`
+    }
+}
+
+class Instructor extends Person {
+    constructor(teach) {
+        super(teach);
+        this.specialty = teach.specialty;
+        this.favLanguage = teach.favLanguage;
+        this.catchPhrase = teach.catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject) {
+        return `${student.name} receives a perfect score on ${subject}.`
+    }
+}
+
+class Student extends Person {
+    constructor(learn) {
+        super(learn);
+        this.previousBackground = learn.previousBackground;
+        this.className = learn.className;
+        this.favSubjects = learn.favSubjects;
+    }
+    listSubjects() {
+        return `${this.favSubjects}`
+    }
+    PRAssignment(subject) {
+        return `${this.name} has begun sprint challenge on ${subject}.`
+    }
+    sprintChallenge(subject) {
+        return `${this.name} has begun their sprint challenge on ${subject}.`
+    }
+}
+
+class ProjectManagers extends Instructor {
+    constructor(manage) {
+        super(manage);
+        this.gradClassName = manage.gradClassName;
+        this.favInstructor = manage.favInstructor;
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!`
+    }
+    debugsCode(student, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}.`
     }
 }
 
